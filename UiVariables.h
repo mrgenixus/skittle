7b0bcd62979fbd6bd2c87705447379d5993e29e1
Benjamin West <mrgenixus@gmail.com>
2011-08-17 22:49:05 -0600

    Prototyped header

--- UiVariables.h
+++ UiVariables.h
@@ -12,18 +12,12 @@ class UiVariables : public QObject
 	Q_OBJECT
 		
 public:
-    QSpinBox* sizeDial;
-    QSpinBox* widthDial;
-    QSpinBox* startDial;
-    QSpinBox* scaleDial;
-    QSpinBox* zoomDial;
-    QSpinBox* offsetDial;
-	QTextEdit* textArea;
 
 	UiVariables();
 	UiVariables(QTextEdit*,bool nullInstance = true);
 	//UiVariables(const UiVariables& copy);
     //UiVariables(const UiVariables* copy);
+    copyValues(const UiVariables* );
 
 public slots:
 	void print(const char*);
@@ -35,11 +29,60 @@ public slots:
     void updateDisplay();
 	//void print(int num1, int num2); 
     
+    
+    void setSizeDial(QSpinBox*);
+    QSpinBox* getSizeDial();
+    void setSize(int);
+    int getSize();
+    
+    void setWidthDial(QSpinBox*);
+    QSpinBox* getWidthDial();
+    void setWidth(int);
+    int getWidth();
+    
+    void setStartDial(QSpinBox*);
+    QSpinBox* getStartDial();
+    void setStart(int);
+    int getStart();
+    
+    void setScaleDial(QSpinBox* );
+    QSpinBox* getScaleDial();
+    void setScale(int);
+    int getScale();
+    
+    void setZoomDial(QSpinBox* );
+    QSpinBox* getZoomDial();
+    void setZoom(int);
+    int getZoom();
+    
+    void setOffsetDial(QSpinBox* );
+    QSpinBox* getOffsetDial();
+    void setOffset(int);
+    int getOffset();
+    
+    void setTextArea(QTextEdit*);
+    QString getOutput();
+    
 signals:
     void StateChanged(UiVariables*);
+    void sizeChanged(int);
+    void widthChanged(int);
+    void startChanged(int);
+    void scaleChanged(int);
+    void zoomChanged(int);
+    void offsetChanged(int);
+    void textPrinted(QString);
     
 private:
     void createConnections();
+
+    QSpinBox* sizeDial;
+    QSpinBox* widthDial;
+    QSpinBox* startDial;
+    QSpinBox* scaleDial;
+    QSpinBox* zoomDial;
+    QSpinBox* offsetDial;
+	QTextEdit* textArea;
 
 };
 #endif
